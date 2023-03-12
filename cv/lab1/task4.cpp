c082e521d27826255e63612bb162258894c5be00
Alberto Zerbinati <albz@bendingspoons.com>
2023-03-12 11:02:10 +0000

    [lab 1] complete

--- cv/lab1/task4.cpp
+++ cv/lab1/task4.cpp
@@ -1,28 +1,61 @@
-#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/highgui.hpp>
+
+bool areArgumentsEnough(int argc, int n);
+bool areChannelsEnough(int channels, int n);
+bool isImageValid(cv::Mat& img);
+cv::Mat createSingleChannelImage(int channel, cv::Mat& img);
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "\nusage: intro.exe <img_path>\n\n";
+    if (!areArgumentsEnough(argc, 2)) {
+        std::cout << "\nusage: intro.exe <img_path>\n";
         return 1;
     }
-    cv::Mat img = cv::imread(argv[1]);
 
-    if (img.data == NULL) {
-        std::cout << "\n" << argv[1] << " is not a vaild path for an image\n\n";
+    cv::Mat img = cv::imread(argv[1]);
+    if (!isImageValid(img)) {
+        std::cout << "\n" << argv[1] << " is not a vaild path for an image\n";
         return 1;
     }
 
     int nChannels = img.channels();
     std::cout << "\nthe image has " << nChannels << " channels\n";
+    if (!areChannelsEnough(nChannels, 3)) {
+        std::cout << "\n image has too few channels, needs 3 instead they are "
+                  << nChannels << "\n";
+        return 1;
+    }
 
-    // TODO
+    cv::Mat firstChannelImage = createSingleChannelImage(0, img);
+    cv::Mat secondChannelImage = createSingleChannelImage(1, img);
+    cv::Mat thirdChannelImage = createSingleChannelImage(2, img);
 
     cv::namedWindow("Lab 1");
+    cv::namedWindow("Lab 1.1");
+    cv::namedWindow("Lab 1.2");
+    cv::namedWindow("Lab 1.3");
     cv::imshow("Lab 1", img);
-    
+    cv::imshow("Lab 1.1", firstChannelImage);
+    cv::imshow("Lab 1.2", secondChannelImage);
+    cv::imshow("Lab 1.3", thirdChannelImage);
+
     char c = cv::waitKey(0);
     std::cout << "\nyou pressed the " << c << " key\n";
 
     return 0;
+}
+
+bool areArgumentsEnough(int argc, int n) { return argc >= n; }
+bool areChannelsEnough(int channels, int n) { return channels >= n; }
+bool isImageValid(cv::Mat& img) { return img.data != NULL; }
+cv::Mat createSingleChannelImage(int channel, cv::Mat& img) {
+    cv::Mat ret = cv::Mat(img.rows, img.cols, img.type());
+
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            ret.at<cv::Vec3b>(i, j)[channel] = img.at<cv::Vec3b>(i, j)[channel];
+        }
+    }
+
+    return ret;
 }
--- cv/lab1/task5.cpp
+++ cv/lab1/task5.cpp
@@ -1,8 +1,72 @@
-#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/highgui.hpp>
+
+void fillWithVerticalGradient(cv::Mat& img);
+void fillWithHorizontalGradient(cv::Mat& img);
+void createChessboardPattern(int squareSize, cv::Mat& img);
 
 int main(int argc, char** argv) {
-    // TODO
+    cv::Mat imgVerticalGradient = cv::Mat(256, 256, CV_8UC1);
+    cv::Mat imgHorizontalGradient = cv::Mat(256, 256, CV_8UC1);
+
+    fillWithVerticalGradient(imgVerticalGradient);
+    fillWithHorizontalGradient(imgHorizontalGradient);
+
+    cv::Mat chessboard1 = cv::Mat(300, 300, CV_8UC1);
+    cv::Mat chessboard2 = cv::Mat(300, 300, CV_8UC1);
+
+    createChessboardPattern(20, chessboard1);
+    createChessboardPattern(50, chessboard2);
+
+    cv::namedWindow("Lab 1.1");
+    cv::namedWindow("Lab 1.2");
+    cv::namedWindow("Lab 1.3");
+    cv::namedWindow("Lab 1.4");
+    cv::imshow("Lab 1.1", imgVerticalGradient);
+    cv::imshow("Lab 1.2", imgHorizontalGradient);
+    cv::imshow("Lab 1.3", chessboard1);
+    cv::imshow("Lab 1.4", chessboard2);
+
+    char c = cv::waitKey(0);
+    std::cout << "\nyou pressed the " << c << " key\n";
 
     return 0;
+}
+
+void fillWithVerticalGradient(cv::Mat& img) {
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            img.at<unsigned char>(i, j) = i;
+        }
+    }
+}
+
+void fillWithHorizontalGradient(cv::Mat& img) {
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            img.at<unsigned char>(i, j) = j;
+        }
+    }
+}
+
+void createChessboardPattern(int squareSize, cv::Mat& img) {
+    unsigned char color = 255;
+    unsigned char initialColor = color;
+    for (int i = 0; i < img.rows; ++i) {
+        color = initialColor;
+        for (int j = 0; j < img.cols; ++j) {
+            if (j != 0 && j % squareSize == 0) {
+                if (color == 255)
+                    color = 0;
+                else
+                    color = 255;
+            }
+            img.at<unsigned char>(i, j) = color;
+        }
+        if (i != 0 && i % squareSize == 0)
+            if (initialColor == 255)
+                initialColor = 0;
+            else
+                initialColor = 255;
+    }
 }
