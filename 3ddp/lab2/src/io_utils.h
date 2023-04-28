aad8b9f6f05228d6aa8f1aef01b8acce7367267b
Alberto Zerbinati <albz@bendingspoons.com>
2023-04-28 09:03:19 +0000

    [3d lab2] refactor style

--- 3ddp/lab2/src/io_utils.h
+++ 3ddp/lab2/src/io_utils.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
-bool readFileNamesFromFolder ( const std::string& input_folder_name, std::vector< std::string >& names );
-bool loadCameraParams( const std::string &file_name, cv::Size &image_size,
-                       cv::Mat &camera_matrix, cv::Mat &dist_coeffs );
+bool readFileNamesFromFolder(const std::string &input_folder_name,
+                             std::vector<std::string> &names);
+bool loadCameraParams(const std::string &file_name, cv::Size &image_size,
+                      cv::Mat &camera_matrix, cv::Mat &dist_coeffs);
