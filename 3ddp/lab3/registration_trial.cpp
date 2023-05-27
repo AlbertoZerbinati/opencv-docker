edb966409c15f2c86e785acd7033614af9b10f9b
Alberto Zerbinati <zerbi.albi@gmail.com>
2023-05-27 18:00:22 +0200

    [3ddp lab3] measure run time

--- 3ddp/lab3/registration_trial.cpp
+++ 3ddp/lab3/registration_trial.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 
 #include "Registration.h"
@@ -8,7 +9,18 @@ int main(int argc, char *argv[]) {
     Registration registration(argv[1], argv[2]);
     registration.draw_registration_result();
 
+    std::chrono::steady_clock::time_point begin =
+        std::chrono::steady_clock::now();
     registration.execute_icp_registration(0.2, 100, 1e-6, mode);
+    std::chrono::steady_clock::time_point end =
+        std::chrono::steady_clock::now();
+    std::cout << "\nElapsed time = "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       begin)
+                         .count() /
+                     1000.0
+              << "[s]" << std::endl;
+
     registration.draw_registration_result();
     std::cout << "RMSE: " << registration.compute_rmse() << std::endl;
     registration.write_tranformation_matrix("transformation.txt");
